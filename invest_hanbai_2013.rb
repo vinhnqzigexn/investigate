de9471f7c0baa7cdf183823d2a6140797ec408f9
vinhnq <vinhnq@zigexn.vn>
2018-07-12 17:40:08 +0700

    update invest 2013

---
+++ invest_hanbai_2013.rb
@@ -0,0 +1,10 @@
+株式会社ロフティー（N14215）
+
+
+30 * * * *
+
+minute  - hour  - day - month - dayofweek
+
+minute: 30 
+
+https://tecadmin.net/crontab-in-linux-with-20-examples-of-cron-schedule/
---
+++ sankou_new_layout.rb
@@ -0,0 +1,7 @@
+3	Layout adjustments
+3.1	Logo replacement
+3.2	Text contents
+3.3	Design
+3.4	Intergration test
+
+https://kyujinjoho.biz/ https://hanbai.in/
